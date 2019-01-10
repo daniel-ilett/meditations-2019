09bcfbac8fc36cfd0fb9790f1f1588e518912925
Daniel Ilett <danielilett@gmail.com>
2019-01-10 23:33:12 +0000

    Fully implemented Player animations.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
 	private Vector2 moveVelocity;
 
 	private const float moveSpeed = 2.5f;
 
+	private Animator animator;
 	private new Rigidbody2D rigidbody;
 
 	private void Awake()
 	{
+		animator = GetComponent<Animator>();
 		rigidbody = GetComponent<Rigidbody2D>();
 	}
 
@@ -27,6 +30,19 @@ public class Player : MonoBehaviour
 		{
 			moveVelocity.Normalize();
 		}
+
+		// Set the player orientation based on movement direction.
+		if(Mathf.Abs(moveVelocity.y) > Mathf.Abs(moveVelocity.x))
+		{
+			animator.SetInteger("Direction", (moveVelocity.y > 0.0f) ? 2 : 1);
+		}
+		else if(Mathf.Abs(moveVelocity.x) > Mathf.Abs(moveVelocity.y))
+		{
+			animator.SetInteger("Direction", (moveVelocity.x < 0.0f) ? 0 : 3);
+		}
+
+		// Set the walk animation status accordingly.
+		animator.SetBool("IsWalk", moveVelocity.magnitude > 0.5f);
 	}
 
 	private void FixedUpdate()
